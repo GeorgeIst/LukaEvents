9ee22fa67f402583aa41b4920be55a7f9cf16879
GeorgeIst <istrate.george.e@gmail.com>
2019-12-01 13:31:02 +0200

    Test git

---
+++ src/test/java/com/luka/sda/eventServiceSystem/controller/EventControllerTest.java
@@ -0,0 +1,12 @@
+package com.luka.sda.eventServiceSystem.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EventControllerTest {
+
+    @Test
+    public void newEvent() {
+    }
+}
